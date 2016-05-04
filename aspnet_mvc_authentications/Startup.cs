09cce9f660e9cc64065b964b90a15f68bafe9bb2
Adarsh Kumar <adarshdch@gmail.com>
2016-05-04 11:22:17 +0530

    Initial Draft

---
+++ aspnet_mvc_authentications/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(aspnet_mvc_authentications.Startup))]
+namespace aspnet_mvc_authentications
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
